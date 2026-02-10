f375d3a24332b19fd9200f270721c4f790533b17
PRAGYA KUMARI <pragya91kumari@gmail.com>
2026-02-11 00:00:48 +0530

    minmax

--- Exp2.py
+++ Exp2.py
@@ -1,13 +1,16 @@
-arr = [1, 4, 3, 5, 8, 6]
-
-minimum = arr[0]
-maximum = arr[0]
-
-for i in arr:
-    if i < minimum:
-        minimum = i
-    if i > maximum:
-        maximum = i
-
-print("Minimum:", minimum)
-print("Maximum:", maximum)
+def find_min_max():
+    n = int(input("Enter number of elements: "))
+    arr = []
+    for i in range(n):
+        num = int(input("Enter element: "))
+        arr.append(num)
+    minimum = arr[0]
+    maximum = arr[0]
+    for x in arr:
+        if x < minimum:
+            minimum = x
+        if x > maximum:
+            maximum = x
+    print("Minimum =", minimum)
+    print("Maximum =", maximum)
+find_min_max()
